a7208a4e588469c8ffb0b5e7f4845df89c3b828a
kpujjigit <103009467+kpujjigit@users.noreply.github.com>
2023-03-09 18:48:22 -0800

    Added Actions screen to manually invoke issue and performance events

---
+++ EmpowerPlant/RandomErrors.swift
@@ -0,0 +1,8 @@
+//
+//  RandomErrors.swift
+//  EmpowerPlant
+//
+//  Created by Karan Pujji on 2/20/23.
+//
+
+import Foundation
---
+++ EmpowerPlant/SentryTransactionTests.swift
@@ -0,0 +1,8 @@
+//
+//  SentryTransactionTests.swift
+//  EmpowerPlant
+//
+//  Created by Karan Pujji on 2/20/23.
+//
+
+import Foundation
